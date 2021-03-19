90c197b1ef51be191adefd95e71c2ef24f8edd1f
Don <don_nguyen2001@yahoo.com>
2021-03-19 10:16:19 -0700

    update

--- GraphQL/GraphQLSchema.cs
+++ GraphQL/GraphQLSchema.cs
@@ -8,6 +8,10 @@ namespace GraphQL_API.GraphQL
 {
     public class GraphQLSchema:Schema, ISchema
         {
+          public GraphQLSchema(IDependencyResolver resolver):base(resolver)
+            {
+                Query = resolver.Resolve<FactInterventionQuery>();
 
+            }
         }
 }
